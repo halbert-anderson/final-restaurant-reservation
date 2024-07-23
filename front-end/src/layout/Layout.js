d84b65ad74ff2d53bde92630a8aa46f273609230
Hal Anderson <halbert.anderson@verizon.net>
2024-07-23 17:23:41 -0400

    updated menu,layout and style.css files so that the nav bar always spans the entore height of the screen

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -1,26 +1,21 @@
 import React from "react";
 import Menu from "./Menu";
 import Routes from "./Routes";
-
 import "./Layout.css";
 
 /**
  * Defines the main layout of the application.
  *
- * You will not need to make changes to this file.
- *
  * @returns {JSX.Element}
  */
 function Layout() {
   return (
-    <div className="container-fluid">
-      <div className="row h-100">
-        <div className="col-md-2 side-bar">
-          <Menu />
-        </div>
-        <div className="col">
-          <Routes />
-        </div>
+    <div className="main-container">
+      <div className="sidebar">
+        <Menu />
+      </div>
+      <div className="content">
+        <Routes />
       </div>
     </div>
   );
